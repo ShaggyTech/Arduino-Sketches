112812d9efb4784361d0148333b638ccf6bfc13b
Shaggy <beedat1@gmail.com>
2015-04-11 00:48:30 -0500

    Completed V1.0, fixed file structure

--- Flex_Sensor_Min_Max/Flex_Sensor_Min_Max.ino
+++ Flex_Sensor_Min_Max/Flex_Sensor_Min_Max.ino
@@ -1,15 +1,34 @@
 /*
-SparkFun Inventor's Kit
-Example sketch 09
+---------------------------------------------------------------------------------------
 
-FLEX SENSOR
+Flex_Sensor_Min_Max by Brandon Eichler
+Version 1.0
 
-  Use the "flex sensor" to change the position of a servo
-  
-  In the previous sketch, we learned how to command a servo to
-  mode to different positions. In this sketch, we'll introduce
-  a new sensor, and use it to control the servo.
-  
+Source Code: https://github.com/vdubmastertech/Arduino-Sketches
+
+Some portions of this sketch were originally written by SparkFun Electronics,
+with lots of help from the Arduino community.
+See:  https://github.com/sparkfun/SIK-Guide-Code/blob/master/Circuit_09/Circuit_09.ino
+      http://learn.sparkfun.com/products/2 for SIK information.
+      http://www.arduino.cc to learn about the Arduino.
+
+This code is completely free for any use.
+
+----------------------------------------------------------------------------------------
+
+** SKETCH PURPOSE:
+  Focuses on logging the minimum and maximum readings of a flex sensor.
+  This is especially useful if you want to use the map() function on the flex
+  sensor reading and need exact upper/lower limits of your specific flex sensor.
+
+** INSTRUCTIONS:
+  All you need to do is flex the sensor back and forth and eventually the display
+  values will not change, these final readings are the upper/lower limits of 
+  that specific flex sensor.  If you want to test another sensor, simply replace
+  the current sensor with the new one and reset your arduino board.  Then repeat
+  the above to get the new readings.
+
+** FLEX SENSOR
   A flex sensor is a plastic strip with a conductive coating.
   When the strip is straight, the coating will be a certain
   resistance. When the strip is bent, the particles in the coating
@@ -18,76 +37,42 @@ FLEX SENSOR
   animals, etc. See http://www.sparkfun.com/tutorials/270 for
   more information.
   
-Hardware connections:
-
-  Flex sensor:
-
-    The flex sensor is the plastic strip with black stripes.
-    It senses bending away from the striped side.
-    
-    The flex sensor has two pins, and since it's a resistor,
-    the pins are interchangable.
-    
-    Connect one of the pins to ANALOG IN pin 0 on the Arduino.
-    Connect the same pin, through a 10K Ohm resistor (brown
-    black orange) to GND.
-    Connect the other pin to 5V.
-
-  Servo:
+** SETTING UP THE FLEX SENSOR
+  The flex sensor is the plastic strip with black stripes.
+  It senses bending away from the striped side.
   
-    The servo has a cable attached to it with three wires.
-    Because the cable ends in a socket, you can use jumper wires
-    to connect between the Arduino and the servo. Just plug the
-    jumper wires directly into the socket.
-    
-    Connect the RED wire (power) to 5 Volts (5V)
-    Connect the WHITE wire (signal) to digital pin 9
-    Connect the BLACK wire (ground) to ground (GND)
+  The flex sensor has two pins, and since it's a resistor,
+  the pins are interchangable.
   
-    Note that servos can use a lot of power, which can cause your
-    Arduino to reset or behave erratically. If you're using large
-    servos or many of them, it's best to provide them with their
-    own separate 5V supply. See this Arduino Forum thread for info:
-    http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1239464763
-
-This sketch was written by SparkFun Electronics,
-with lots of help from the Arduino community.
-This code is completely free for any use.
-Visit http://learn.sparkfun.com/products/2 for SIK information.
-Visit http://www.arduino.cc to learn about the Arduino.
-
-Version 2.0 6/2012 MDG
+  Connect one of the pins to ANALOG IN pin 0 on the Arduino.
+  Connect the same pin, through a 10K Ohm resistor (brown
+  black orange) to GND.
+  Connect the other pin to 5V.
+  
+--------------------------------------------------------------------------------------
 */
 
 
-// Include the servo library to add servo-control functions:
-
-#include <Servo.h> 
-
-// Create a servo "object", called servo1. Each servo object
-// controls one servo (you can have a maximum of 12):
-
-Servo servo1;
-
 // Define the analog input pin to measure flex sensor position:
+#define flexpin 0
 
-const int flexpin = 0; 
+// Initial values for min and max variables
+int minFlex = 790;
 int maxFlex = 800;
-int minFlex = 700;
-int maxServo = 60;
-int minServo = 50;
 
 void setup() 
 { 
-  // Use the serial monitor window to help debug our sketch:
-   
+  // Use the serial monitor window to log the flex sensor values:
   Serial.begin(9600);
-  
-  // Enable control of a servo on pin 9:
-
-  servo1.attach(9);
 } 
 
+/* 
+This function takes a single argument, the current flex sensor reading "x", 
+and compares it to current values of the minFlex and maxFlex variables.
+
+In other words, it stores the highest and lowest values that
+are sensed from the flex sensor and prints them out if the min/max has changed.
+*/
 void minMaxFlex(int x){
   if (x < minFlex){
     minFlex = x;
@@ -104,70 +89,21 @@ void minMaxFlex(int x){
   }
 }
 
-void minMaxServo(int x){
-  if (x < minServo){
-    minServo = x;
-    printMinMax();
-    return;
-  }
-  else if (x > maxServo){
-    maxServo = x;
-    printMinMax();
-    return;
-  }
-  else {
-    return;
-  }
-}
-
+// Logs the flex sensor min max readings to the serial terminal.
 void printMinMax(){
-  Serial.print("maxFlex:  ");
+  Serial.print("maxFlex: ");
   Serial.print(maxFlex);
-  Serial.print("  minFlex:  ");
-  Serial.print(minFlex);
-  Serial.print("  maxServo:  ");
-  Serial.print(maxServo);
-  Serial.print("  minServo:  ");
-  Serial.println(minServo);
+  Serial.print("  minFlex: ");
+  Serial.println(minFlex);
   return;
 }
 
 void loop() 
 { 
   int flexPosition;    // Input value from the analog pin.
-  int servoPosition;   // Output value to the servo.
   
-  // Read the position of the flex sensor (0 to 1023):
-  
+  // Read the position of the flex sensor (0 to 1023):  
   flexPosition = analogRead(flexpin);
   
-  minMaxFlex(flexPosition);
-  minMaxServo(servoPosition);
-
-  // Because the voltage divider circuit only returns a portion
-  // of the 0-1023 range of analogRead(), we'll map() that range
-  // to the servo's range of 0 to 180 degrees. The flex sensors
-  // we use are usually in the 600-900 range:
-  
-  servoPosition = map(flexPosition, minFlex, maxFlex, 0, 180);
-  servoPosition = constrain(servoPosition, 0, 180);
-
-  // Now we'll command the servo to move to that position:
-
-  servo1.write(servoPosition);
-
-  // Because every flex sensor has a slightly different resistance,
-  // the 600-900 range may not exactly cover the flex sensor's
-  // output. To help tune our program, we'll use the serial port to
-  // print out our values to the serial monitor window:
-  
-  //Serial.print("sensor: ");
-  //Serial.print(flexposition);
-  //Serial.print("  servo: ");
-  //Serial.println(servoposition);
-  
-  delay(20);  // wait 20ms between servo updates
+  minMaxFlex(flexPosition); // checks and (if needed) updates the maxFlex and minFlex values
 } 
-
-
-
